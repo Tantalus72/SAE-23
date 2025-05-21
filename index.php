6b8ce7aae8ed74f469e53d1efab7c33be4874cb6
kali <kali@kali.home>
2025-05-21 08:56:32 +0200

    init

---
+++ index.php
@@ -0,0 +1,33 @@
+<html lang="fr">
+<head>
+<script src=""></script>
+<link red="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+
+
+
+
+</header>
+
+
+<?php
+
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+<footer>
+
+
+</footer>
+</html>
