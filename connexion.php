75117ac2237775236fe5037777876bda8b1be057
Kyonew <tiziano.senes.fr@icloud.com>
2025-05-27 15:22:07 +0200

    log

--- connexion.php
+++ connexion.php
@@ -13,10 +13,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     if(!empty($_POST['email']) && !empty($_POST['password'])) {
         if(login($_POST['email'], $_POST['password'])) {
             $_SESSION['email'] = $_POST['email'];
-            $fichier = fopen('../logs/access.log','a+');
+            /*$fichier = fopen('../logs/access.log','a+');
             fputs($fichier,$_POST['email']." de ".$_SERVER['REMOTE_ADDR']." à ".date('l jS\of F Y h:i:s A')." sous le status admin ? :".isAdmin($_SESSION["email"])." s'est connecté");
             fputs($fichier,"\n");
-            fclose($fichier);
+            fclose($fichier);*/
+            $fichier = fopen('../logs/access.log','a+');
+            fputs($fichier,'ecrit');
             header('Location: index.php');
             exit();
         } else {
--- insertion.php
+++ insertion.php
@@ -42,9 +42,36 @@ try {
             }
         }
 
+        // Check if file already exists
+        if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+        }
+
+
+        if ($uploadOk == 1) {
+            if (move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
+                echo "The file ". htmlspecialchars( basename( $_FILES["img"]["name"])). " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
 
         // Validation des données
-        if (empty($idMarque) || empty($idModele) || empty($designation)) {
+        if (empty($idMarque) || empty($idModele) || empty($designation) || $uploadOk != 1) {
             throw new Exception("Tous les champs obligatoires doivent être remplis");
         }
 
@@ -59,7 +86,8 @@ try {
             $designation,
             $annee,
             $kilometrage,
-            $prix
+            $prix,
+            $_FILES["img"]["name"]
         ]);
 
         $success = "Annonce créée avec succès !";
